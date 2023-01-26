1ddab0f5ad1c96e4993b43a535bd095382585a5a
Liam Berigan <liamaberigan@gmail.com>
2023-01-26 09:53:45 -0500

    Updated the visualization apps to show tables

--- classifier_integrated/visualization/primary_state_app.R
+++ classifier_integrated/visualization/primary_state_app.R
@@ -1,6 +1,8 @@
+
 library(shiny)
 library(tidyverse)
 library(leaflet)
+library(excelR)
 
 
 # Define UI for application that draws a histogram
@@ -10,13 +12,15 @@ ui <- fluidPage(
            leafletOutput(outputId = "plot",height = 700),
            h3(textOutput("compile_status"), align = "center"),
            actionButton("goButton", "Next"), p("Proof the next individual")
-    )
-
-))
+    ),
+    column(width = 6,
+           excelOutput("table", height = 400))
+    
+  ))
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
+  
   ### OK, lets intersect the location data with the shapefile for the AMWO 
   ### SGS coverage zones
   
@@ -27,10 +31,11 @@ server <- function(input, output) {
   amwoData.sm <- readRDS(here::here("classifier_integrated", "fac_primary_state_delineation.rds"))
   
   amwoData.sm %>%
-    mutate(animal_name = as.character(animal_name)) %>%
-    mutate(point_state = primary_point_state) %>%
-    mutate(x = long, y = lat, date = as.Date(time)) %>%
-    arrange(time) ->
+    mutate(animal_name = as.character(animal_name),
+           x = long,
+           y = lat,
+           date = as.Date(time),
+           point_state = primary_point_state) ->
     amwoData.sm
   
   individual_stepper <- reactiveValues() #These values can be defined w/in a reactive expression and will be remembered in other reactive expressions
@@ -39,6 +44,13 @@ server <- function(input, output) {
   individual_stepper$current_id <- unique(amwoData.sm$animal_name)[1]
   individual_stepper$amwoDataID <- amwoData.sm %>%
     filter(animal_name == unique(amwoData.sm$animal_name)[1])
+  
+  
+  selected_columns <- filter(amwoData.sm, animal_name == unique(amwoData.sm$animal_name)[1]) %>% 
+    dplyr::select("animal_name", "primary_point_state", "time") 
+  
+  output$table <- excelTable(data = selected_columns, tableHeight = "800px") %>% 
+    renderExcel()
   
   getColor <- function(state) {
     sapply(state$point_state, function(states) { # individual_stepper$amwoDataID$point_state
@@ -54,11 +66,11 @@ server <- function(input, output) {
   }
   
   individual_stepper$icons <- awesomeIcons(
-      icon = 'ios-close',
-      iconColor = 'black',
-      library = 'ion',
-      markerColor = unname(getColor(filter(amwoData.sm, animal_name == unique(amwoData.sm$animal_name)[1])))
-    )
+    icon = 'ios-close',
+    iconColor = 'black',
+    library = 'ion',
+    markerColor = as.character(getColor(filter(amwoData.sm, animal_name == unique(amwoData.sm$animal_name)[1])))
+  )
   
   
   #Reactive plotting: anything within this expression reruns every time input is modified
@@ -77,15 +89,15 @@ server <- function(input, output) {
                         popup = individual_stepper$amwoDataID$date) %>%  
       addPolylines(lng =individual_stepper$amwoDataID$x, 
                    lat = individual_stepper$amwoDataID$y, 
-                   weight=3, color="purple")
-
+                   weight=3, color="red")
+    
     
   })#end of reactive plotting
   
   #clean locations that we start with
-#  location_iterator <- reactive({
-    #amwoData.sm %>%  filter(ID == individual_stepper$current_ID)
-#  }) 
+  #  location_iterator <- reactive({
+  #amwoData.sm %>%  filter(animal_name == individual_stepper$current_ID)
+  #  }) 
   
   #updating when go button is pressed
   observeEvent(input$goButton, {
@@ -99,19 +111,23 @@ server <- function(input, output) {
       individual_stepper$compiled <- 1
     }
     
-    individual_stepper$amwoDataID <- subset(amwoData.sm, 
-                                            amwoData.sm$animal_name==individual_stepper$current_id)
-
+    individual_stepper$amwoDataID <- subset(amwoData.sm, amwoData.sm$animal_name==individual_stepper$current_id)
+    
+    selected_columns <- individual_stepper$amwoDataID %>% 
+      dplyr::select("animal_name", "primary_point_state", "time")  
+    
+    output$table <- renderExcel(excelTable(data = selected_columns, tableHeight = "800px"))
+    
     individual_stepper$icons <- awesomeIcons(
       icon = 'ios-close',
       iconColor = 'black',
       library = 'ion',
-      markerColor = unname(getColor(individual_stepper$amwoDataID))
+      markerColor = getColor(individual_stepper$amwoDataID)
     )
   })
   
-#  observeEvent(input$goButton,{
-#  })individual_stepper$current_id
+  #  observeEvent(input$goButton,{
+  #  })individual_stepper$current_id
   
   output$compile_status <- renderText({
     if(individual_stepper$compiled == 0){
