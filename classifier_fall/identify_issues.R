c6d85a9dc73b00263569dbff9751b18445540201
Liam Berigan <liamaberigan@gmail.com>
2023-03-15 17:35:57 -0400

    Implemented estimable initial state
    Haven't checked this yet to look at potential issues. Worried that this might put migratory locations in places that shouldn't be migratory; considering a date cutoff to redesignate the initial point state if the first point came before a certain date. Issues with incorrect state designations (see identify_issues.R in the fall folder) remain.

--- classifier_fall/identify_issues.R
+++ classifier_fall/identify_issues.R
@@ -1,9 +1,29 @@
 fall_short_mig <- fall_classified %>% 
   filter(step_state == 2) %>%
-  filter(step < 30.2) %>% View()
+  filter(step < 30.2) %>% 
   nrow()
 
 fall_long_stationary <- fall_classified %>% 
+  filter(step_state %in% c(1,3,4)) %>%
+  filter(step > 30.2) %>% 
+  nrow()
+
+spring_f_short_mig <- spring_female_classified %>% 
+  filter(step_state == 2) %>%
+  filter(step < 30.2) %>% 
+  nrow()
+
+spring_f_long_stationary <- spring_female_classified %>% 
+  filter(step_state %in% c(1,3,4)) %>%
+  filter(step > 30.2) %>% 
+  nrow()
+
+spring_m_short_mig <- spring_male_classified %>% 
+  filter(step_state == 2) %>%
+  filter(step < 30.2) %>% 
+  nrow()
+
+spring_m_long_stationary <- spring_male_classified %>% 
   filter(step_state %in% c(1,3,4)) %>%
   filter(step > 30.2) %>% 
   nrow()
