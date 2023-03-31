6aee5cf8830358ffcbccae96c491bdc57b155c16
Liam Berigan <liamaberigan@gmail.com>
2023-03-30 23:30:41 -0400

    CHECKPOINT: Ready for advisor review
    Bug fixes implemented, closes #16, closes #17. Ready for advisor review.

--- classifier_integrated/compile_results/spring_foray_loops.R
+++ classifier_integrated/compile_results/spring_foray_loops.R
@@ -3,7 +3,7 @@ library(tidyverse)
 
 amwo_data_reconstructed <- readRDS(here::here("classifier_integrated", "fac_primary_state_delineation.rds"))
 
-#
+#FL-2021-01
 #starts on 2021-05-19, ends on 2021-05-27
 # foray loop steps: 18843090289, 18843090301
 # foray loop points: 18843090301
--- classifier_integrated/compile_results/winter_foray_loops.R
+++ classifier_integrated/compile_results/winter_foray_loops.R
@@ -20,4 +20,42 @@ amwo_data_reconstructed <- amwo_data_reconstructed %>%
   mutate(primary_step_state = if_else(event_id %in% c(8262529843, 8262529844, 8340308849), "Foray loop (winter)", primary_step_state),
          primary_point_state = if_else(event_id %in% c(8262529844, 8340308849), "Foray loop (winter)", primary_point_state))
 
+#NY-2018-04 (removing foray loop as the bird doesn't settle)
+#starts on 2018-11-21, ends on 2018-11-24
+# foray loop steps: 8037640949, 8037640950, 8007051304
+# foray loop points: 8037640950, 8007051304
+# amwo_data_reconstructed <- amwo_data_reconstructed %>% 
+#   mutate(primary_step_state = if_else(event_id %in% c(8037640949, 8037640950, 8007051304), "Foray loop (winter)", primary_step_state),
+#          primary_point_state = if_else(event_id %in% c(8037640950, 8007051304), "Foray loop (winter)", primary_point_state))
+
+#RI-2021-46
+#starts on 2022-02-10, ends on 2022-02-16
+# foray loop steps: 21279049764, 21279049767, 21279049774
+# foray loop points: 21279049767, 21279049774
+amwo_data_reconstructed <- amwo_data_reconstructed %>% 
+  mutate(primary_step_state = if_else(event_id %in% c(21279049764, 21279049767, 21279049774), "Foray loop (winter)", primary_step_state),
+         primary_point_state = if_else(event_id %in% c(21279049767, 21279049774), "Foray loop (winter)", primary_point_state))
+
+
+
+#NJ-2018-03
+#starts on 2019-1-10, ends on 2019-1-23
+# foray loop steps: 8807074660, 8807074661
+# foray loop points: 8807074661
+
+amwo_data_reconstructed <- amwo_data_reconstructed %>% 
+  mutate(primary_step_state = if_else(event_id %in% c(8807074660, 8807074661), "Foray loop (winter)", primary_step_state),
+         primary_point_state = if_else(event_id %in% c(8807074661), "Foray loop (winter)", primary_point_state))
+
+
+#PA-2019-15
+#starts on 2019-12-10, ends on 2019-12-16
+# foray loop steps: 13598268172, 13598268173, 13607659369
+# foray loop points: 13598268173, 13607659369
+
+amwo_data_reconstructed <- amwo_data_reconstructed %>% 
+  mutate(primary_step_state = if_else(event_id %in% c(13598268172, 13598268173, 13607659369), "Foray loop (winter)", primary_step_state),
+         primary_point_state = if_else(event_id %in% c(13598268173, 13607659369), "Foray loop (winter)", primary_point_state))
+
+
 saveRDS(amwo_data_reconstructed, file = here::here("classifier_integrated", "fac_primary_state_delineation.rds"))
