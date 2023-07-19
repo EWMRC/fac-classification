ad368159934a3cf5c14acba3c85a807ec88085c2
Liam Berigan <liamaberigan@gmail.com>
2023-07-19 11:04:19 -0400

    Edits made to individual birds
    Changes have all been added to the paper appendix. Done all checks but the integrated general visualization app.

--- classifier_integrated/compile_results/fall_foray_loops.R
+++ classifier_integrated/compile_results/fall_foray_loops.R
@@ -27,5 +27,28 @@ amwo_data_reconstructed <- amwo_data_reconstructed %>%
   mutate(primary_step_state = if_else(event_id %in% c(20247710616, 20247710619, 20334121982, 20334121986), "Foray loop", primary_step_state),
          primary_point_state = if_else(event_id %in% c(20247710619, 20334121982, 20334121986), "Foray loop", primary_point_state))
 
+#NY-2018-06 (part 1)
+# starts on 2018-11-17, ends on 2018-11-18
+# foray loop steps: 7977579345, 7994489554
+# foray loop points: 7994489554
+amwo_data_reconstructed <- amwo_data_reconstructed %>% 
+  mutate(primary_step_state = if_else(event_id %in% c(7977579345, 7994489554), "Foray loop", primary_step_state),
+         primary_point_state = if_else(event_id %in% c(7994489554), "Foray loop", primary_point_state))
+
+#NY-2018-06 (part 2)
+# starts on 2018-12-25, ends on through 2018-12-27
+# foray loop steps: 8352992073, 8352992074, 8352992075
+# foray loop points: 8352992074, 8352992075
+amwo_data_reconstructed <- amwo_data_reconstructed %>% 
+  mutate(primary_step_state = if_else(event_id %in% c(8352992073, 8352992074, 8352992075), "Foray loop", primary_step_state),
+         primary_point_state = if_else(event_id %in% c(8352992074, 8352992075), "Foray loop", primary_point_state))
+
+#PA-2021-37
+# starts on 2021-12-06 20:01:04, ends on 2021-12-08 05:00:48
+# foray loop steps: 20994782599, 20994782602
+# foray loop points: 20994782602
+amwo_data_reconstructed <- amwo_data_reconstructed %>% 
+  mutate(primary_step_state = if_else(event_id %in% c(20994782599, 20994782602), "Foray loop", primary_step_state),
+         primary_point_state = if_else(event_id %in% c(20994782602), "Foray loop", primary_point_state))
 
 saveRDS(amwo_data_reconstructed, file = here::here("classifier_integrated", "fac_primary_state_delineation.rds"))
