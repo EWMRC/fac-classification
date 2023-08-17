104baec4d55ee2db45a46375273bb44eba0dd5b2
LiamABerigan <liamaberigan@gmail.com>
2023-08-17 10:23:38 -0400

    Integrated classification check
    Checked all tracks in the integrated classifier and fixed several associated bugs. Uploaded most recent version (w/ 16.1km threshold) to Movebank.

--- classifier_integrated/compile_results/spring_foray_loops.R
+++ classifier_integrated/compile_results/spring_foray_loops.R
@@ -12,4 +12,13 @@ amwo_data_reconstructed <- amwo_data_reconstructed %>%
   mutate(primary_step_state = if_else(event_id %in% c(18843090289, 18843090301), "Foray loop", primary_step_state),
          primary_point_state = if_else(event_id %in% c(18843090301), "Foray loop", primary_point_state))
 
+#VA-2020-66
+#starts on 2021-01-25, ends on 2021-02-03
+# foray loop steps: 17816584331, 17816584333
+# foray loop points: 17816584333
+
+amwo_data_reconstructed <- amwo_data_reconstructed %>% 
+  mutate(primary_step_state = if_else(event_id %in% c(17816584331, 17816584333), "Foray loop", primary_step_state),
+         primary_point_state = if_else(event_id %in% c(17816584333), "Foray loop", primary_point_state))
+
 saveRDS(amwo_data_reconstructed, file = here::here("classifier_integrated", "fac_primary_state_delineation.rds"))
