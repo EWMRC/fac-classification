66681e5848938f7c3de1acae6f029fffca567855
LiamABerigan <liamaberigan@gmail.com>
2024-09-07 16:31:05 -0400

    2024 update
    Good chance that there will be bug fixes, but for now at least we have a 2024 update on Movebank.

--- classifier_integrated/compile_results/summer_dispersal_movements.R
+++ classifier_integrated/compile_results/summer_dispersal_movements.R
@@ -10,12 +10,24 @@ amwo_data_reconstructed <- readRDS(here::here("classifier_integrated", "fac_prim
 amwo_data_reconstructed <- amwo_data_reconstructed %>% 
   mutate(primary_step_state = if_else(event_id == 11644874412, "Dispersal", primary_step_state))
 
-#VA-20202-52
+#VA-2020-52
 #No dispersal points, just a step
 #starts on 2020-06-19, ends on 2020-06-28
 #15096830096
 amwo_data_reconstructed <- amwo_data_reconstructed %>% 
   mutate(primary_step_state = if_else(event_id == 15096830096, "Dispersal", primary_step_state))
+
+#WV-2024-47
+#replacing spring mig with a dispersal
+#32983659939
+amwo_data_reconstructed <- amwo_data_reconstructed %>% 
+  mutate(primary_step_state = if_else(event_id == 32983659939, "Dispersal", primary_step_state))
+
+#WV-2024-55
+#replacing spring mig with a dispersal
+#33815123757
+amwo_data_reconstructed <- amwo_data_reconstructed %>% 
+  mutate(primary_step_state = if_else(event_id == 33815123757, "Dispersal", primary_step_state))
 
 
 saveRDS(amwo_data_reconstructed, file = here::here("classifier_integrated", "fac_primary_state_delineation.rds"))
--- classifier_integrated/compile_results/summer_migrations.R
+++ classifier_integrated/compile_results/summer_migrations.R
@@ -27,4 +27,20 @@ amwo_data_reconstructed <- amwo_data_reconstructed %>%
   mutate(primary_step_state = if_else(event_id %in% c(19589114101, 19589114109), "Migratory (summer)", primary_step_state),
          primary_point_state = if_else(event_id == 19589114109, "Migratory (summer)", primary_point_state))
 
+#WV-2024-27
+# starts on 2024-07-13, ends on 
+# migratory steps: 34106450785, 34200333436, 34200333443
+# migratory points: 34200333436, 34200333443
+amwo_data_reconstructed <- amwo_data_reconstructed %>% 
+  mutate(primary_step_state = if_else(event_id %in% c(34106450785, 34200333436, 34200333443), "Migratory (summer)", primary_step_state),
+         primary_point_state = if_else(event_id %in% c(34200333436, 34200333443), "Migratory (summer)", primary_point_state))
+
+#WV-2024-41
+# starts on 2024-07-07, ends on 
+# migratory steps: 
+# migratory points: 
+amwo_data_reconstructed <- amwo_data_reconstructed %>% 
+  mutate(primary_step_state = if_else(event_id %in% c(34016350099, 34106450750, 34106450775), "Migratory (summer)", primary_step_state),
+         primary_point_state = if_else(event_id %in% c(34106450750, 34106450775), "Migratory (summer)", primary_point_state))
+
 saveRDS(amwo_data_reconstructed, file = here::here("classifier_integrated", "fac_primary_state_delineation.rds"))
